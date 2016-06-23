d73efba2deb46efca50f426668022509ef3766d8
Brock Fanning <brockfanning@gmail.com>
2016-06-23 11:41:28 -0400

    Fixes and reporting improvements

--- src/Document.php
+++ src/Document.php
@@ -18,8 +18,34 @@ class Document {
   private $relativePrefix;
   private $tempPath;
   private $text;
+  private $errorMessages;
+  private $absolutePath;
 
-  public function __construct($document, $relativePrefix, $tempPath = '/tmp/keywordcrawler') {
+  public function getRelativePrefix() {
+    return $this->relativePrefix;
+  }
+
+  public function getTempPath() {
+    return $this->tempPath;
+  }
+
+  public function getErrorMessages() {
+    return $this->errorMessages;
+  }
+
+  public function addErrorMessage($message) {
+    $this->errorMessages[] = $message;
+  }
+
+  public function getDocument() {
+    return $this->document;
+  }
+
+  public function getAbsolutePath() {
+    return $this->absolutePath;
+  }
+
+  public function __construct($document, $relativePrefix, $tempPath) {
 
     $this->document = $document;
     $this->relativePrefix = $relativePrefix;
@@ -35,12 +61,26 @@ class Document {
 
     // Add an http prefix if the document is only a relative link.
     if (strpos($document, 'http') !== 0) {
-      $document = $this->relativePrefix . $document;
+      $document = $this->getRelativePrefix() . rawurlencode($document);
     }
-    $response = file_get_contents($document, false, $context);
+
+    // Save this full path for error messages later.
+    $this->absolutePath = $document;
+
+    try {
+      $response = @file_get_contents($document, false, $context);
+      if ($response === FALSE) {
+        if (!empty($http_response_header[0])) {
+          $this->addErrorMessage($http_response_header[0]);
+        }
+      }
+    }
+    catch (\Exception $e) {
+      $this->addErrorMessage($e->getMessage());
+    }
 
     // Save the file locally to make it easier to do the other things.
-    file_put_contents($this->tempPath, $response);
+    file_put_contents($this->getTempPath(), $response);
   }
 
   private function getText() {
@@ -66,12 +106,13 @@ class Document {
         $this->text = $reader->output();
       }
       catch (\Exception $e) {
-
+        $this->addErrorMessage($e->getMessage());
       }
     }
   }
 
   public function getKeywords() {
+
     $keywords = '';
     $this->getText();
     try {
@@ -82,8 +123,9 @@ class Document {
       $keywords = implode(' ', array_keys($keywords));
     }
     catch (\Exception $e) {
-
+      $this->addErrorMessage($e->getMessage());
     }
+
     return $keywords;
   }
 
--- src/KeywordCrawler.php
+++ src/KeywordCrawler.php
@@ -6,7 +6,7 @@
 
 namespace USDOJ\SingleTableFacets;
 
-use \USDOJ\SingletableFacets\Document;
+use \USDOJ\SingleTableFacets\Document;
 
 class KeywordCrawler {
 
@@ -19,7 +19,7 @@ class KeywordCrawler {
 
   // When the app is instantiated, we can do all of the expensive things once
   // and then store them on the object.
-  public function __construct($db, $table, $urlColumn, $keywordColumn, $relativePrefix, $tempPath = '') {
+  public function __construct($db, $table, $urlColumn, $keywordColumn, $relativePrefix, $tempPath = '/tmp/keywordcrawler') {
 
     $this->db = $db;
     $this->table = $table;
@@ -57,24 +57,76 @@ class KeywordCrawler {
     $query = $this->getDb()->createQueryBuilder();
     $query->from($this->getTable());
     $query->select($this->getUrlColumn());
+    $query->where($query->expr()->lte('LENGTH(' . $this->getKeywordColumn() . ')', 0));
     if ($limit != -1) {
       $query->setMaxResults($limit);
       $query->setFirstResult($offset);
     }
     $result = $query->execute();
-    foreach ($result as $row) {
-      $url = $row[$this->getUrlColumn()];
-      if (!empty($url)) {
-        $document = new Document($url, $this->getRelativePrefix(), $this->getTempPath());
-        $keywords = $document->getKeywords();
-        if (!empty($keywords)) {
-          $update = $this->getDb()->createQueryBuilder();
-          $update->update($this->getTable(), $this->getTable())
-            ->set($this->getKeywordColumn(), $keywords)
-            ->where("{$this->getUrlColumn()} = $url");
-          $affected = $update->execute();
+    $successes = $failures = array();
+    if (!empty($result)) {
+      foreach ($result as $row) {
+        $url = $row[$this->getUrlColumn()];
+        $newValue = '';
+        if (!empty($url)) {
+          $document = new Document($url, $this->getRelativePrefix(), $this->getTempPath());
+          $keywords = $document->getKeywords();
+          $errors = $document->getErrorMessages();
+          if (empty($errors) && !empty($keywords)) {
+            $newValue = $keywords;
+            $successes[] = sprintf('-- Updated keywords for %s.', $document->getAbsolutePath());
+          }
+          else {
+            $failure = '-- ' . $document->getAbsolutePath();
+            if (!empty($errors)) {
+              foreach ($errors as $error) {
+                if (!empty($error)) {
+                  $failure .= PHP_EOL . '   ' . $error;
+                }
+              }
+            }
+            $failures[] = $failure;
+
+            // So that we don't keep trying to do these, we actually save a
+            // value in the column. But we keep it short and consistent so we
+            // can search it intentionally.
+            $newValue = 'crawling error';
+          }
+
+          // Update the database.
+          if (!empty($newValue)) {
+            $update = $this->getDb()->createQueryBuilder();
+            $update->update($this->getTable(), $this->getTable())
+              ->set($this->getKeywordColumn(), ':keywords')
+              ->where($update->expr()->eq($this->getUrlColumn(), ':url'))
+              ->setParameter(':keywords', $newValue)
+              ->setParameter(':url', $url);
+            $affected = $update->execute();
+          }
         }
       }
     }
+    else {
+      print sprintf('No more keywords need to be updated.');
+      print PHP_EOL;
+    }
+
+    if (!empty($successes)) {
+      print PHP_EOL . '**** Successes ****' . PHP_EOL . PHP_EOL;
+      foreach ($successes as $success) {
+        print $success . PHP_EOL;
+      }
+      print PHP_EOL . PHP_EOL;
+    }
+    if (!empty($failures)) {
+      print '**** Failures ****' . PHP_EOL . PHP_EOL;
+      foreach ($failures as $failure) {
+        print $failure . PHP_EOL;
+      }
+    }
+
+    print PHP_EOL . 'Totals:' . PHP_EOL;
+    print count($successes) . ' success.' . PHP_EOL;
+    print count($failures) . ' failures.' . PHP_EOL;
   }
 }
