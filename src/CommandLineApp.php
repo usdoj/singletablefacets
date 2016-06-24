ea185ca3232ffaf6754593b2e50e01e0e7d6fe8f
Brock Fanning <brockfanning@gmail.com>
2016-06-24 14:54:41 -0400

    Starting to look at config objects and yaml

---
+++ src/CommandLineApp.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Class for preparing for usage of SingleTableFacets from the command line.
+ */
+
+namespace USDOJ\SingleTableFacets;
+
+class CommandLineApp
+{
+
+    private $config;
+    private $db;
+
+    public function __construct($config)
+    {
+
+    }
+}
---
+++ src/Config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * Class for configuration options for SingleTableFacets.
+ */
+
+namespace USDOJ\SingleTableFacets;
+
+class Config extends \Noodlehaus\Config
+{
+
+}
