f05eeb2c6af71020f1d7cf68d7852396c2d7fb1e
Brock Fanning <brockfanning@gmail.com>
2016-06-28 23:24:09 -0400

    Default values

--- src/Config.php
+++ src/Config.php
@@ -8,5 +8,28 @@ namespace USDOJ\SingleTableFacets;
 
 class Config extends \Noodlehaus\Config
 {
-
+    protected function getDefaults() {
+        return array(
+            'minimum valid keyword length' => 3,
+            'show counts next to facet items' => TRUE,
+            'search button text' => 'Search',
+            'no results message' => '<p>Sorry, no results could be found for those keywords.</p>',
+            'use checkboxes for facets instead of links' => TRUE,
+            'number of items per page' => 20,
+            'number of pager links to show' => 5,
+            'show dependents indented to the right' => TRUE,
+            'keyword help' => '
+                <ul>
+                    <li>Use the checkboxes on the left to refine your search, or enter new keywords to start over.</li>
+                    <li>Enter multiple keywords to get fewer results, eg: cat dogs</li>
+                    <li>Use OR to get more results, eg: cats OR dogs</li>
+                    <li>Put a dash (-) before a keyword to exclude it, eg: dogs -lazy</li>
+                    <li>Use "" (double-quotes) to match specific phrases, eg: "the brown fox"</li>
+                </ul>
+            ',
+            'keyword help label' => "Need help searching?",
+            'sort facet items by popularity' =>  FALSE,
+            'remove common keywords' => TRUE,
+        );
+    }
 }
