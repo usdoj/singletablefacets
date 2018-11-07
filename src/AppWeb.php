608df44852d1ad1459b9f52214e02c8f5dccdb07
Johann Huang <johann.huang@usdoj.gov>
2018-11-06 20:30:44 -0500

    Rename config parameter

--- src/AppWeb.php
+++ src/AppWeb.php
@@ -417,8 +417,8 @@ class AppWeb extends \USDOJ\SingleTableFacets\App {
 
         $keywords = $this->getParameter('keys');
 
-        $pattern = array_keys($this->settings('keywords alternatives'));
-        $replace = array_values($this->settings('keywords alternatives'));
+        $pattern = array_keys($this->settings('regex input alternation'));
+        $replace = array_values($this->settings('regex input alternation'));
         $keywords = preg_replace($pattern, $replace, $keywords);
 
         $tokenized = $this->tokenizeQuoted($keywords);
